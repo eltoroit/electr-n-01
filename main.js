f8dbf28de13f56de783a5d735e54c0694abee90c
Andres Perez <aperez@salesforce.com>
2022-09-27 15:38:49 -0400

    Window's like in Mac

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 ECMAScript modules (i.e. using import to load a module) are currently not directly supported in Electron.
 You can find more information about the state of ESM in Electron in https://github.com/electron/electron/issues/21457
 */
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-    })
-    
-    win.loadFile('index.html')
-}
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+	});
+
+	win.loadFile("index.html");
+};
 
 /*
 You typically listen to Node.js events by using an emitter's .on function.
@@ -21,6 +21,21 @@ However, Electron exposes app.whenReady() as a helper specifically for the ready
 pitfalls with directly listening to that event in particular. See https://github.com/electron/electron/pull/21972 for details.
 */
 app.whenReady().then(() => {
-    createWindow()
-})
+	createWindow();
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on("window-all-closed", () => {
+	/*
+    Checking against Node's *process.platform* variable can help you to run code conditionally on certain platforms.
+    Note that there are only three possible platforms that Electron can run in:
+    - win32 (Windows),
+    - linux (Linux),
+    - darwin (macOS).
+    */
+	if (process.platform !== "darwin") app.quit();
+});
+
 console.log(`Hello from Electron 👋`);
